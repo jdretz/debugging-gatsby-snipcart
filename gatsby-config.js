dd40dc4e0c27d346343aecf5fad34c593ff88a3e
jdretz <jarrett.retz@comcast.net>
2020-12-13 15:35:15 -0800

    add sanity

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,1 +1,11 @@
-module.exports = {};
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: `ah76eszu`,
+        dataset: `production_2`,
+      }
+    }
+  ]
+};
