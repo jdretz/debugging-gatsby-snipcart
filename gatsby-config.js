dbcc683a4d3821b6a1b92ee111b0c766239f5d11
jdretz <jarrett.retz@comcast.net>
2020-12-13 13:36:27 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "ah76eszu",
+        dataset: "",
+      },
+    },
+    "gatsby-plugin-postcss",
+  ],
+};
